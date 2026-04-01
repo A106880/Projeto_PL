e272540ac2b63da6db035ee967bcc50757bffacc
Afonso_Q <169604828+A106880@users.noreply.github.com>
2026-04-01 23:16:11 +0100

    Adição de funções de conversão de tokens

--- main.py
+++ main.py
@@ -1,22 +1,90 @@
 import ply.lex as lex
 
 class LexError(Exception):
-  pass
+    pass
 
 literals = "()\n,\t"
 tokens = ('PROGRAM', 'ID', 'END', 'FUNCTION', 'INTEGER', 'INTVAL', 'REAL', 'REALVAL', 'DOUBLEPRECISION', 'DOUBLEPRECISIONVAL', 'COMPLEX', 'COMPLEXVAL', 'DOUBLECOMPLEX', 'DOUBLECOMPLEXVAL', 'LOGICAL', 'LOGICALVAL', 'CHARACTER', 'CHARACTERVAL', 'HOLLERITH', 'HOLLERITHVAL', 'PRINT', 'READ', 'WRITE', 'DO', 'MOD', 'IF', 'THEN', 'ELSE', 'ENDIF', 'GOTO', 'CONTINUE')
 
-def t_INT(t):
-  r'\d+'
-  t.value = int(t.value)
-  return t
+def t_INTVAL(t):
+    r'\d+'
+    t.value = int(t.value)
+    return t
 
+def t_DOUBLEPRECISIONVAL(t):
+    r'\d+\.\d*(?:[Dd][+-]?\d+)?|\.\d+(?:[Dd][+-]?\d+)?|\d+[Dd][+-]?\d+'
+    t.value = float(t.value.lower().replace('d', 'e'))
+    return t
+
+def t_REALVAL(t):
+    r'\d+\.\d*(?:[Ee][+-]?\d+)?|\.\d+(?:[Ee][+-]?\d+)?|\d+[Ee][+-]?\d+'
+    t.value = float(t.value)
+    return t
+
+def t_COMPLEXVAL(t):
+    pass
+
+def t_DOUBLECOMPLEXVAL(t):
+    pass
+
+def t_LOGICALVAL(t):
+    r'\.(TRUE|true|FALSE|false)\.'
+    t.value = True if 'true' in t.value.lower() else False
+    return t
+
+def t_CHARACTERVAL(t):
+    r"'([^']|'')*'"
+    t.value = t.value[1:-1].replace("''", "'")
+    return t
+
+
+def t_HOLLERITHVAL(t):
+    pass
+
+
+reserved = {
+    'PROGRAM': 'PROGRAM', 
+    'END': 'END', 
+    'FUNCTION': 'FUNCTION', 
+    'INTEGER': 'INTEGER', 
+    'REAL': 'REAL', 
+    'DOUBLEPRECISION': 'DOUBLEPRECISION',
+    'COMPLEX': 'COMPLEX',
+    'DOUBLECOMPLEX': 'DOUBLECOMPLEX',
+    'LOGICAL': 'LOGICAL', 
+    'CHARACTER': 'CHARACTER',
+    'HOLLERITH': 'HOLLERITH',
+    'PRINT': 'PRINT', 
+    'READ': 'READ', 
+    'WRITE': 'WRITE',
+    'DO': 'DO', 
+    'MOD': 'MOD', 
+    'IF': 'IF', 
+    'THEN': 'THEN', 
+    'ELSE': 'ELSE', 
+    'ENDIF': 'ENDIF', 
+    'GOTO': 'GOTO', 
+    'CONTINUE': 'CONTINUE' 
+}
+
+def t_ID(t):
+    r'[a-zA-Z_][a-zA-Z_0-9]*'
+    t.type = reserved.get(t.value.upper(), 'ID')
+    return t
 
 t_ignore = ' \t\n'
 
+def t_COMMENT(t):
+    r'(^[Cc*].*)|(!.*)'
+    pass
+
+
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += len(t.value)
 
 def t_error(t):
-  raise LexError(f"Illegal character {t.value[0]}")
+    raise LexError(f"Illegal character {t.value[0]}")
 
 
 __file__ = "Untitled.ipynb"
@@ -27,27 +95,27 @@ _lookahead = None
 
 
 def next_token():
-  global _lookahead
-  _lookahead = lexer.token()
+    global _lookahead
+    _lookahead = lexer.token()
 
 
 def lookahead():
-  return (_lookahead.type, _lookahead.value, _lookahead.lineno, _lookahead.lexpos) if _lookahead else ('$', None, None, None)
+    return (_lookahead.type, _lookahead.value, _lookahead.lineno, _lookahead.lexpos) if _lookahead else ('$', None, None, None)
 
 
 class ParserError(Exception):
-  pass
+    pass
 
 
 def recognize_terminal(expected_type):
-  token_type, token_val, token_line, token_pos = lookahead()
+    token_type, token_val, token_line, token_pos = lookahead()
 
 
-  if(token_type == expected_type):
-    next_token()
-    return token_val
-  else:
-    raise ParserError(f'Unexpected token when recognizing terminal {expected_type}: {token_type}')
+    if(token_type == expected_type):
+        next_token()
+        return token_val
+    else:
+        raise ParserError(f'Unexpected token when recognizing terminal {expected_type}: {token_type}')
 
 
 
@@ -151,7 +219,7 @@ def recognize_VarType():
 def recognize_Val():
     token_type, token_val, token_line, token_pos = lookahead()
 
-    first_Val = ['ID', 'INTEGER', 'REAL', 'DOUBLEPRECISION', 'COMPLEX', 'DOUBLECOMPLEX', 'LOGICAL', 'CHARACTER', 'HOLLERITH']
+    first_Val = ['ID', 'INTVAL', 'REALVAL', 'DOUBLEPRECISIONVAL', 'COMPLEXVAL', 'DOUBLECOMPLEXVAL', 'LOGICALVAL', 'CHARACTERVAL', 'HOLLERITHVAL']
 
     if token_type in first_Val:
         recognize_terminal(token_type)
