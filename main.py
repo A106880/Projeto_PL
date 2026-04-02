8ccc2067a59b7a7abd9692bd5fcea95e1a914c83
Afonso_Q <169604828+A106880@users.noreply.github.com>
2026-04-02 19:52:06 +0100

    Adicao de novas regras de gramatica e novas funções do compilador

--- main.py
+++ main.py
@@ -7,30 +7,81 @@ class Node:
         self.lineno = None
 
 class ProgramaPrincipal(Node):
-    def __init__(self, funcoes_cima, nome, declaracoes, comandos, funcoes_baixo):
+    def __init__(self, upper_functions, name, declarations, labeled_statements, bottom_functions):
         super().__init__()
         self.tipo_no = 'PROGRAMA_PRINCIPAL'
-        self.funcoes_cima = funcoes_cima
-        self.nome = nome
-        self.declaracoes = declaracoes
-        self.comandos = comandos
-        self.funcoes_baixo = funcoes_baixo
+        self.upper_functions = upper_functions
+        self.name = name
+        self.declarations = declarations
+        self.labeled_statements = labeled_statements
+        self.bottom_functions = bottom_functions
 
     def __repr__(self):
-        return f"Programa({self.nome})"
+        return (f" Funções(cima){self.upper_functions}\n"
+                f"Programa({self.name})\n"
+                f"  Declarações{self.declarations}\n"
+                f"  LabeledStatements{self.labeled_statements}\n"
+                f"  Funções(baixo){self.bottom_functions}")
 
 class Funcao(Node):
-    def __init__(self, tipo_retorno, nome, argumentos, declaracoes, comandos):
+    def __init__(self, return_type, name, arguments, declarations, labeled_statements):
         super().__init__()
-        self.tipo_retorno = tipo_retorno
-        self.nome = nome
-        self.argumentos = argumentos
-        self.declaracoes = declaracoes
-        self.comandos = comandos
+        self.return_type = return_type
+        self.name = name
+        self.arguments = arguments
+        self.declarations = declarations
+        self.labeled_statements = labeled_statements
         
     def __repr__(self):
-        return f"Funcao({self.nome})"
+        return (f"Funcao({self.name}, TipoRetorno: {self.return_type})\n"
+                f"  Argumentos: {self.arguments}\n"
+                f"  {self.declarations}\n"
+                f"  {self.labeled_statements}")
 
+class DoublePrecisonComplexVal(Node):
+    def __init__(self, elem1, elem2):
+        super().__init__()
+        self.elem1 = elem1
+        self.elem2 = elem2
+    def __repr__(self):
+        return f"DoublePrecisionComplex({self.elem1},{self.elem2})"
+
+class ComplexVal(Node):
+    def __init__(self, elem1, elem2):
+        super().__init__()
+        self.elem1 = elem1
+        self.elem2 = elem2
+    def __repr__(self):
+        return f"Complex({self.elem1},{self.elem2})"
+    
+class Print(Node):
+    def __init__(self,format,iolist):
+        super().__init__()
+        self.format = format
+        self.iolist = iolist
+    def __repr__(self) -> str:
+        return f"Print(Format: {self.format}, Items: {self.iolist})"
+
+class BinOp(Node):
+    """Nó para operações binárias (ex: A + B, NUM .GT. 5)"""
+    def __init__(self, left, op, right):
+        super().__init__()
+        self.left = left
+        self.op = op
+        self.right = right
+        
+    def __repr__(self):
+        return f"({self.left} {self.op} {self.right})"
+
+class UnOp(Node):
+    """Nó para operações unárias (ex: -A, .NOT. ISPRIM)"""
+    def __init__(self, op, expr):
+        super().__init__()
+        self.op = op
+        self.expr = expr
+        
+    def __repr__(self):
+        return f"({self.op} {self.expr})"
 
 class Declaracao(Node):
     def __init__(self, tipo, ArrayIdList):
@@ -40,8 +91,10 @@ class Declaracao(Node):
     
     def __repr__(self):
         str = f"Declaracao({self.tipo}"
-        for elem in self.ArrayIdList:
-            str = str + f"{elem.nome}[{elem.tamanho}] / "
+        for elem in self.Ids:
+            str += f" {elem.nome}"
+            str += (f"[{elem.tamanho}]" if elem.tamanho > 0 else "")
+            str += " / "
         return str + ")"
 
 
@@ -50,21 +103,28 @@ class ArrayId(Node):
     def __init__(self,nome,tamanho):
         super().__init__()
         self.nome = nome
-        if(tamanho == None):
+        if(tamanho is None):
             self.tamanho = 0 # ou 1 caso se queira fazer singleton == array de 1 elemento 
-        self.tamanho = tamanho
-
+        else:
+            self.tamanho = tamanho
+    def __repr__(self):
+        # Faltava-te o __repr__ aqui! 
+        # Assim ele distingue se é uma variável simples (N) ou um array (NUMS[5])
+        if self.tamanho and self.tamanho != 0:
+            return f"{self.nome}[{self.tamanho}]"
+        return f"{self.nome}"
 
 class LexError(Exception):
     pass
 
-literals = "(),\t"
+literals = "(),+-*/"
 tokens = (
     'NEWLINE', 'PROGRAM', 'ID', 'END', 'FUNCTION', 'INTEGER', 'INTVAL', 
     'REAL', 'REALVAL', 'DOUBLEPRECISION', 'DOUBLEPRECISIONVAL', 'COMPLEX', 
-    'COMPLEXVAL', 'DOUBLECOMPLEX', 'DOUBLECOMPLEXVAL', 'LOGICAL', 'LOGICALVAL', 
+    'DOUBLECOMPLEX', 'LOGICAL', 'LOGICALVAL', 
     'CHARACTER', 'CHARACTERVAL', 'HOLLERITH', 'HOLLERITHVAL', 'PRINT', 'READ', 
-    'WRITE', 'DO', 'MOD', 'IF', 'THEN', 'ELSE', 'ENDIF', 'GOTO', 'CONTINUE'
+    'WRITE', 'DO', 'MOD', 'IF', 'THEN', 'ELSE', 'ENDIF', 'GOTO', 'CONTINUE', 
+    'POWER', 'CONCAT', 'AND', 'OR', 'NOT', 'EQ', 'NE', 'LT', 'LE', 'GT', 'GE'
 )
 
 reserved = {
@@ -107,14 +167,6 @@ def t_REALVAL(t):
     t.value = float(t.value)
     return t
 
-def t_COMPLEXVAL(t):
-    r'COMPLEXVAL_PLACEHOLDER'
-    pass
-
-def t_DOUBLECOMPLEXVAL(t):
-    r'DOUBLECOMPLEXVAL_PLACEHOLDER'
-    pass
-
 def t_LOGICALVAL(t):
     r'\.(TRUE|true|FALSE|false)\.'
     t.value = True if 'true' in t.value.lower() else False
@@ -127,34 +179,9 @@ def t_CHARACTERVAL(t):
 
 
 def t_HOLLERITHVAL(t):
-    r'HOLLERITHVAL_PLACEHOLDER'
+    r'\d+[Hh]\w*'
     pass
 
-
-reserved = {
-    'PROGRAM': 'PROGRAM', 
-    'END': 'END', 
-    'FUNCTION': 'FUNCTION', 
-    'INTEGER': 'INTEGER', 
-    'REAL': 'REAL', 
-    'DOUBLEPRECISION': 'DOUBLEPRECISION',
-    'COMPLEX': 'COMPLEX',
-    'DOUBLECOMPLEX': 'DOUBLECOMPLEX',
-    'LOGICAL': 'LOGICAL', 
-    'CHARACTER': 'CHARACTER',
-    'HOLLERITH': 'HOLLERITH',
-    'PRINT': 'PRINT', 
-    'READ': 'READ', 
-    'WRITE': 'WRITE',
-    'DO': 'DO', 
-    'MOD': 'MOD', 
-    'IF': 'IF', 
-    'THEN': 'THEN', 
-    'ELSE': 'ELSE', 
-    'ENDIF': 'ENDIF', 
-    'GOTO': 'GOTO', 
-    'CONTINUE': 'CONTINUE' 
-}
 
 def t_ID(t):
     r'[a-zA-Z_][a-zA-Z_0-9]*'
@@ -174,30 +201,86 @@ def t_NEWLINE(t):
     t.type = 'NEWLINE'
     return t
 
+def t_POWER(t):
+    r'\*\*'
+    return t
+
+def t_CONCAT(t):
+    r'//'
+    return t
+
+def t_AND(t):
+    r'\.AND\.|\.and\.'
+    return t
+
+def t_OR(t):
+    r'\.or\.|\.OR\.'
+    return t
+
+def t_NOT(t):
+    r'\.NOT\.|\.not\.'
+    return t
+
+def t_EQ(t):
+    r'\.EQ\.|\.eq\.'
+    return t
+
+def t_NE(t):
+    r'\.NE\.|\.ne\.'
+    return t
+
+def t_LT(t):
+    r'\.LT\.|\.lt\.'
+    return t
+
+def t_LE(t):
+    r'\.LE\.|\.le\.'
+    return t
+
+def t_GT(t):
+    r'\.GT\.|\.gt\.'
+    return t
+
+def t_GE(t):
+    r'\.GE\.|\.ge\.'
+    return t
+
 def t_error(t):
     raise LexError(f"Illegal character {t.value[0]}")
 
 lexer = lex.lex()
 
-# p1: Main -> Functions PROGRAM ID\n Declaritions Statements END\n Functions
+precedence = (
+    ('left', 'OR'),            # .OR.
+    ('left', 'AND'),           # .AND.
+    ('right', 'NOT'),          # .NOT.
+    ('left', 'EQ', 'NE', 'LT', 'LE', 'GT', 'GE'), # Relacionais
+    ('left', 'CONCAT'),        # //
+    ('left', '+', '-'),        # Adição e Subtração
+    ('left', '*', '/'),        # Multiplicação e Divisão
+    ('right', 'POWER'),        # ** (Exponenciação)
+    ('right', 'UMINUS'),       # Operador Unário (Negativo)
+)
+
+# p1: Main -> Functions PROGRAM ID\n Declaritions LabeledStatements END\n Functions
 def p_main(p):
-    '''Main : Functions PROGRAM ID NEWLINE Declarations Statements END NEWLINE Functions'''
+    '''Main : Functions PROGRAM ID NEWLINE Declarations LabeledStatements END NEWLINE Functions'''
     
     p[0] = ProgramaPrincipal(
-        funcoes_cima=p[1],
-        nome=p[3],
-        declaracoes=p[5],
-        comandos=p[6],
-        funcoes_baixo=p[9]
+        upper_functions=p[1],
+        name=p[3],
+        declarations=p[5],
+        labeled_statements=p[6],
+        bottom_functions=p[9]
     ) 
 
-# p2: Functions -> FunctionType FUNCTION ID (ArgumentList)\n Declaritions Statements END\n Functions
+# p2: Functions -> FunctionType FUNCTION ID (ArgumentList)\n Declaritions LabeledStatements END\n Functions
 # p3:            | Vazio
 def p_functions(p):
-    '''Functions : FunctionType FUNCTION ID '(' ArgumentList ')' NEWLINE Declarations Statements END NEWLINE Functions
+    '''Functions : FunctionType FUNCTION ID '(' ArgumentList ')' NEWLINE Declarations LabeledStatements END NEWLINE Functions
                  | empty'''
     if len(p) > 2:
-        nova_funcao = Funcao(tipo_retorno=p[1], nome=p[3], argumentos=p[5], declaracoes=p[8], comandos=p[9])
+        nova_funcao = Funcao(return_type=p[1], name=p[3], arguments=p[5], declarations=p[8], labeled_statements=p[9])
         p[0] = [nova_funcao] + (p[12] if p[12] else [])
     else:
         p[0] = []
@@ -255,13 +338,29 @@ def p_val(p):
     '''Val : INTVAL
            | REALVAL
            | DOUBLEPRECISIONVAL
-           | COMPLEXVAL
-           | DOUBLECOMPLEXVAL
+           | ComplexVal
+           | DoubleComplexVal
            | LOGICALVAL
            | CHARACTERVAL
            | HOLLERITHVAL
-           | ID'''
+           | ID '''
     p[0] = p[1]
+
+def p_double_complex_val(p):
+    '''DoubleComplexVal : '(' DOUBLEPRECISIONVAL ',' DOUBLEPRECISIONVAL ')' '''
+    doubleComplexValue = DoublePrecisonComplexVal(
+        p[2],
+        p[4]
+    )
+    p[0] = doubleComplexValue
+
+def p_complex_val(p):
+    '''ComplexVal : '(' REALVAL ',' REALVAL ')' '''
+    complexValue = ComplexVal(
+        p[2],
+        p[4]
+    )
+    p[0] = complexValue
     
 # p30: ArgumentList -> ID ArgumentRestList
 # p31:     | Vazio
@@ -287,8 +386,8 @@ def p_argument_rest_list(p):
 #p34: Declarations -> Declaration Declarations 
 #p35:        | Vazio
 def p_Declarations(p):
-    """Declarations : Declaration Declarations"
-                    | empty"""
+    '''Declarations : Declaration Declarations
+                    | empty'''
     
     if len(p) > 2:
         p[0] = [p[1]] + p[2] 
@@ -297,7 +396,7 @@ def p_Declarations(p):
 
 #p36: Declaration -> VarType ArrayIdList\n
 def p_Declaration(p):
-    "Declaration : VarType ArrayIdList"
+    '''Declaration : VarType ArrayIdList NEWLINE'''
     nova_declaracao = Declaracao(p[1],p[2])
     p[0] = nova_declaracao
 
@@ -306,15 +405,15 @@ def p_Declaration(p):
 
 #p37: ArrayIdList -> ID ArraySize ArrayIdListRest
 def p_ArrayIdList(p):
-    "ArrayIdList : ID ArraySize ArrayIdListRest"
+    '''ArrayIdList : ID ArraySize ArrayIdListRest'''
     p[0] = [ArrayId(p[1],p[2])] + p[3]
 
 
 #p38: ArrayIdListRest -> ,ID ArraySize ArrayIdListRest
 #p39:        |Vazio 
 def p_ArrayIdListRest(p): 
-    """ ArrayIdListRest : ',' ID ArraySize ArrayIDListRest
-                        | empty"""
+    ''' ArrayIdListRest : ',' ID ArraySize ArrayIdListRest
+                        | empty'''
     if len(p) > 2:
         p[0] = [ArrayId(p[2],p[3])] + p[4]
     else:
@@ -323,8 +422,8 @@ def p_ArrayIdListRest(p):
 #p40: ArraySize -> (INTVAL)
 #p41:        | Vazio
 def p_ArraySize(p):
-    """ ArraySize : '(' INTVAL ')'
-                  | empty """
+    ''' ArraySize : '(' INTVAL ')'
+                  | empty '''
     if len(p) > 2:
         p[0] = p[2]
     else:
@@ -332,16 +431,78 @@ def p_ArraySize(p):
 
 
 
-#p42: Statements -> Label Statement \n Statements
+#p42: LabeledStatements -> Label Statement \n LabeledStatements
 #p43:                | Vazio
-def p_Statements(p):
-    """Statements : Label Statement '\n' Statements
-                  | empty"""
+def p_LabeledStatements(p):
+    '''LabeledStatements : Label Statement NEWLINE LabeledStatements
+                  | empty'''
     if len(p) > 2:
         p[0] = [(p[1],p[2])] + p[4]
     else:
         p[0] = []
 
+def p_print(p):
+    '''Print : PRINT Format Iolist'''
+    print = Print(
+        format=p[2],
+        iolist=p[3]
+    )
+    p[0] = print
+
+def p_format(p):
+    '''Format : '*'
+              | INTVAL'''
+    p[0] = p[1]
+
+def p_iolist(p):
+    '''Iolist : ',' Expression Iolist
+              | empty'''
+    if len(p)>2:
+        p[0] = [p[2]]+p[3]
+    else:
+        p[0] = []
+
+def p_expression_binop(p):
+    '''Expression : Expression '+' Expression
+                  | Expression '-' Expression
+                  | Expression '*' Expression
+                  | Expression '/' Expression
+                  | Expression POWER Expression
+                  | Expression CONCAT Expression
+                  | Expression EQ Expression
+                  | Expression NE Expression
+                  | Expression LT Expression
+                  | Expression LE Expression  
+                  | Expression GT Expression
+                  | Expression GE Expression
+                  | Expression AND Expression
+                  | Expression OR Expression'''
+    p[0] = BinOp(left=p[1], op=p[2], right=p[3])
+
+def p_expression_unop(p):
+    '''Expression : '-' Expression %prec UMINUS
+                  | '+' Expression %prec UMINUS
+                  | NOT Expression'''
+    p[0] = UnOp(op=p[1], expr=p[2])
+
+def p_expression_group(p):
+    '''Expression : '(' Expression ')' '''
+    p[0] = p[2]
+
+def p_expression_val(p):
+    '''Expression : Val'''
+    p[0] = p[1]
+
+def p_label(p):
+    '''Label : INTVAL
+             | empty'''
+    p[0] = p[1]
+
+def p_statement(p):
+    '''Statement : Print''' 
+    # Por agora, o único comando que o teu parser conhece é o Print. 
+    # Mais tarde vais adicionar aqui o IF, DO, READ, Atribuições, etc.
+    p[0] = p[1]
 
 def p_error(p):
     if p:
@@ -354,3 +515,26 @@ def p_empty(p):
     pass
 
 parser = yacc.yacc()
+
+
+if __name__ == '__main__':
+    # 1. O código Fortran que queres testar
+    # NOTA: Como a tua regra p_main exige um NEWLINE depois do END, 
+    # é crucial deixar uma linha em branco no final da string!
+    codigo_fortran = """PROGRAM HELLO
+PRINT *, 'Ola, Mundo!'
+END
+"""
+
+    print("A iniciar a análise (parsing) do código Fortran...\n")
+    
+    # 2. Chamar o parser
+    # O PLY vai usar o 'lexer' automaticamente por trás dos panos
+    ast = parser.parse(codigo_fortran)
+    
+    # 3. Imprimir o resultado (a tua Árvore de Sintaxe Abstrata)
+    if ast:
+        print("Sucesso! Aqui está a AST gerada:")
+        print(ast)
+    else:
+        print("O parsing falhou e devolveu None. Verifica os erros acima.")
